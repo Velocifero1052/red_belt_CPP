3936f5480f6c0b34fca765864face2f9fc8972cf
Velocifero1052 <commelevent1052@yandex.ru>
2023-06-06 18:04:46 +0500

    max element task completed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,106 @@
-#include <iostream>
+#include "test_runner.h"
 
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <numeric>
+#include <iterator>
+#include <algorithm>
 
+using namespace std;
 
+template<typename ForwardIterator, typename UnaryPredicate>
+ForwardIterator max_element_if(ForwardIterator first, ForwardIterator last, UnaryPredicate pred) {
+
+    auto max = find_if(first, last, pred);
+    if (max == last) {
+        return last;
+    }
+
+    auto curr_position = max;
+
+    while (-1) {
+        advance(curr_position, 1);
+        auto res = find_if(curr_position, last, pred);
+
+        if (res == last) {
+            break;
+        } else {
+            if (*res > *max) {
+                max = res;
+            }
+        }
+    }
+
+    return max;
+}
+
+void TestUniqueMax() {
+    auto IsEven = [](int x) {
+        return x % 2 == 0;
+    };
+
+    const list<int> hill{2, 4, 8, 9, 6, 4, 2};
+    auto max_iterator = hill.begin();
+    advance(max_iterator, 2);
+
+    vector<int> numbers(10);
+    iota(numbers.begin(), numbers.end(), 1);
+
+    Assert(
+            max_element_if(numbers.begin(), numbers.end(), IsEven) == --numbers.end(),
+            "Expect the last element"
+    );
+    Assert(
+            max_element_if(hill.begin(), hill.end(), IsEven) == max_iterator,
+            "Expect the maximal even number"
+    );
+}
+
+void TestSeveralMax() {
+    struct IsCapitalized {
+        bool operator()(const string &s) {
+            return !s.empty() && isupper(s.front());
+        }
+    };
+
+    const forward_list<string> text{"One", "two", "Three", "One", "Two",
+                                    "Three", "one", "Two", "three"};
+    auto max_iterator = text.begin();
+    advance(max_iterator, 4);
+
+    Assert(
+            max_element_if(text.begin(), text.end(), IsCapitalized()) == max_iterator,
+            "Expect thr first \"Two\""
+    );
+}
+
+void TestNoMax() {
+    const vector<int> empty;
+    const string str = "Non-empty string";
+
+    auto AlwaysTrue = [](int) {
+        return true;
+    };
+    Assert(
+            max_element_if(empty.begin(), empty.end(), AlwaysTrue) == empty.end(),
+            "Expect end for empty container"
+    );
+
+    auto AlwaysFalse = [](char) {
+        return false;
+    };
+    Assert(
+            max_element_if(str.begin(), str.end(), AlwaysFalse) == str.end(),
+            "Expect end for AlwaysFalse predicate"
+    );
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TestRunner tr;
+    tr.RunTest(TestUniqueMax, "TestUniqueMax");
+    tr.RunTest(TestSeveralMax, "TestSeveralMax");
+    tr.RunTest(TestNoMax, "TestNoMax");
     return 0;
 }
