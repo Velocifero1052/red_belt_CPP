5c184a624da07e44492486074c18cba46b3cb98a
Velocifero1052 <commelevent1052@yandex.ru>
2023-06-07 10:27:14 +0500

    small refactoring

--- Solutions.h
+++ Solutions.h
@@ -41,16 +41,28 @@ ForwardIterator max_element_if(ForwardIterator first, ForwardIterator last, Unar
     return max;
 }
 
+template <class ForwardIterator, class UnaryPredicate>
+ForwardIterator max_element_if_authors(ForwardIterator first, ForwardIterator last, UnaryPredicate p) {
+    ForwardIterator maxElemIt = find_if(first, last, p);
+    for (ForwardIterator cur = maxElemIt; cur != last; ++cur) {
+        // cur != maxElemIt is checked to avoid re-calculation of p(*maxElemIt) at first iteration
+        if (cur != maxElemIt && p(*cur) && *maxElemIt < *cur) {
+            maxElemIt = cur;
+        }
+    }
+    return maxElemIt;
+}
+
 void TestUniqueMax() {
     auto IsEven = [](int x) {
         return x % 2 == 0;
     };
 
-    const std::list<int> hill{2, 4, 8, 9, 6, 4, 2};
+    const list<int> hill{2, 4, 8, 9, 6, 4, 2};
     auto max_iterator = hill.begin();
     advance(max_iterator, 2);
 
-    std::vector<int> numbers(10);
+    vector<int> numbers(10);
     iota(numbers.begin(), numbers.end(), 1);
 
     Assert(
@@ -61,6 +73,52 @@ void TestUniqueMax() {
             max_element_if(hill.begin(), hill.end(), IsEven) == max_iterator,
             "Expect the maximal even number"
     );
+}
+
+void TestSeveralMax() {
+    struct IsCapitalized {
+        bool operator()(const string &s) {
+            return !s.empty() && isupper(s.front());
+        }
+    };
+
+    const forward_list<string> text{"One", "two", "Three", "One", "Two",
+                                    "Three", "one", "Two", "three"};
+    auto max_iterator = text.begin();
+    advance(max_iterator, 4);
+
+    Assert(
+            max_element_if(text.begin(), text.end(), IsCapitalized()) == max_iterator,
+            "Expect thr first \"Two\""
+    );
+}
+
+void TestNoMax() {
+    const vector<int> empty;
+    const string str = "Non-empty string";
+
+    auto AlwaysTrue = [](int) {
+        return true;
+    };
+    Assert(
+            max_element_if(empty.begin(), empty.end(), AlwaysTrue) == empty.end(),
+            "Expect end for empty container"
+    );
+
+    auto AlwaysFalse = [](char) {
+        return false;
+    };
+    Assert(
+            max_element_if(str.begin(), str.end(), AlwaysFalse) == str.end(),
+            "Expect end for AlwaysFalse predicate"
+    );
+}
+
+void test_max_by_predicate() {
+    TestRunner tr;
+    tr.RunTest(TestUniqueMax, "TestUniqueMax");
+    tr.RunTest(TestSeveralMax, "TestSeveralMax");
+    tr.RunTest(TestNoMax, "TestNoMax");
 }
 
 #endif //RED_BELT_C___SOLUTIONS_H
