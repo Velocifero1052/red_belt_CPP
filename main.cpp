94b69a1f19615e055d245a94bd7ff281822801d0
Velocifero1052 <commelevent1052@yandex.ru>
2023-08-07 18:08:18 +0500

    temp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include <array>
+#include <string_view>
 
+using std::array;
+using std::string_view;
+
+const size_t SIZE = 10;
 
 int main() {
+
+  array<char, SIZE> a = {'a','a','a','a','a','a','a','a','a','a'};
+  auto s = string_view(a.data(), a.size());
+  auto b = string_view(a.data(), a.size());
+  auto c = string_view(a.data(), a.size());
+  auto d = string_view(a.data(), a.size());
+  auto e =  string_view(a.begin());
+  auto f =  string_view(&a[0], a.size());
 
   return 0;
 }
