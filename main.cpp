7a210fcb164bc159a332545a1a7bb8ba6104fc1a
Velocifero1052 <commelevent1052@yandex.ru>
2023-09-28 17:42:59 +0500

    joseph count starters

--- main.cpp
+++ main.cpp
@@ -1,45 +1,118 @@
-#include "profile.h"
-#include <iostream>
-#include <string>
+#include "test_runner.h"
+
+#include <cstdint>
+#include <iterator>
+#include <numeric>
 #include <vector>
-#include <utility>
+#include <queue>
 
 using namespace std;
 
-vector<string> SplitIntoWords(const string& text) {
-  vector<string> words;
+struct NoncopyableInt {
+  int value;
 
-  string current_word;
-  for (const char c: text) {
-    if (c == ' ')
-      words.push_back(move(current_word));
-    else
-      current_word.push_back(c);
+  NoncopyableInt(const NoncopyableInt&) = delete;
+  NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+
+  NoncopyableInt(NoncopyableInt&&) = default;
+  NoncopyableInt& operator=(NoncopyableInt&&) = default;
+};
+
+bool operator == (const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
+  return lhs.value == rhs.value;
+}
+
+ostream& operator << (ostream& os, const NoncopyableInt& v) {
+  return os << v.value;
+}
+
+/*template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+  vector<typename RandomIt::value_type> pool(first, last);
+  size_t cur_pos = 0;
+  while (!pool.empty()) {
+    *(first++) = pool[cur_pos];
+    pool.erase(pool.begin() + cur_pos);
+    if (pool.empty()) {
+      break;
+    }
+    cur_pos = (cur_pos + step_size - 1) % pool.size();
   }
-  words.push_back(current_word);
-  return words;
+}*/
+
+template <typename RandomIt>
+void MakeJosephusPermutationV2(RandomIt first, RandomIt last, uint32_t step_size) {
+  size_t dist = distance(first, last);
+  for (auto it = first;;) {
+
+
+    if (distance(first, it + step_size) > dist) {
+
+    } else {
+
+    }
+  }
 }
 
-string ReadTitle() {
-  return "Hello world!";
+vector<int> MakeTestVector() {
+  vector<int> numbers(10);
+  iota(begin(numbers), end(numbers), 0);
+  return numbers;
 }
 
-uint64_t ReadBigNumber() {
-  return 1000'000'000;
+/*
+void TestIntVector() {
+  const vector<int> numbers = MakeTestVector();
+  {
+    vector<int> numbers_copy = numbers;
+    MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+    ASSERT_EQUAL(numbers_copy, numbers);
+  }
+  {
+    vector<int> numbers_copy = numbers;
+    MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+    ASSERT_EQUAL(numbers_copy, vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
+  }
 }
 
-string ReadFirstName() {
-  return "FirstName";
-}
+void TestAvoidsCopying() {
+  vector<NoncopyableInt> numbers;
+  numbers.push_back({1});
+  numbers.push_back({2});
+  numbers.push_back({3});
+  numbers.push_back({4});
+  numbers.push_back({5});
 
-string ReadLastName() {
-  return "LastName";
+  MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+
+  vector<NoncopyableInt> expected;
+  expected.push_back({1});
+  expected.push_back({3});
+  expected.push_back({5});
+  expected.push_back({4});
+  expected.push_back({2});
+
+  ASSERT_EQUAL(numbers, expected);
 }
+*/
 
 int main() {
-  string first_name = ReadFirstName();
-  string last_name = ReadLastName();
-  string full_name = first_name + last_name;
-  cout << first_name << last_name << endl;
+
+  vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  auto init_size = distance(v.begin(), v.end());
+  auto first = v.begin();
+  auto last = v.end();
+  size_t step_size = 2;
+  auto current = first;
+
+  while (distance(first, current + step_size) <= init_size) {
+
+    cout << *current << endl;
+
+    current += step_size;
+  }
+
+
+
   return 0;
 }
