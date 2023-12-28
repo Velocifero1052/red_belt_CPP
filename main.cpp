8113d71e493268f3c2511fa7c527803eb6fb8069
Velocifero1052 <commelevent1052@yandex.ru>
2023-12-28 17:59:23 +0500

    some temp progress on Josephus Permutation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <numeric>
 #include <vector>
+#include <list>
 #include <utility>
 
 using namespace std;
@@ -18,24 +19,47 @@ struct NoncopyableInt {
 };
 
 template <typename RandomIt>
-void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+void MakeJosephusPermutation2(RandomIt first, RandomIt last, uint32_t step_size) {
   vector<typename RandomIt::value_type> pool(make_move_iterator(first), make_move_iterator(last));
   size_t cur_pos = 0;
   size_t prev_pos;
   auto element_to_remove = pool.begin();
   while (!pool.empty()) {
     *(first++) = std::move(pool[cur_pos]);
-
     (prev_pos < cur_pos) ? element_to_remove += step_size - 1 : element_to_remove = pool.begin() + cur_pos;
-
     pool.erase(element_to_remove);
-    if (pool.empty()) {
-      break;
-    }
+    if (pool.empty()) break;
     prev_pos = cur_pos;
     cur_pos = (cur_pos + step_size - 1) % pool.size();
+
   }
 }
+
+void print_list(const list<int>& l) {
+  for (int i = 0; i < l.size(); i++) {
+    auto it = l.begin();
+    advance(it, i);
+    cout << *(it) << " ";
+  }
+}
+
+template <typename RandomIt>
+void MakeJosephusPermutation_(RandomIt first, RandomIt last, uint32_t step_size) {
+
+  list<typename RandomIt::value_type> pool(make_move_iterator(first), make_move_iterator(last));
+  auto cur_pos = pool.begin();
+  RandomIt prev_pos;
+
+
+
+
+
+  /*for (auto elem: pool) {
+    cout << elem << " ";
+  }
+  cout << endl;*/
+}
+
 
 template <typename T>
 struct Node {
@@ -45,29 +69,30 @@ struct Node {
 };
 
 template <typename RandomIt>
-void MakeJosephusPermutation2(RandomIt first, RandomIt last, uint32_t step_size) {
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
 
   auto size = distance(first, last);
   if (size == 0 || size == 1)
     return;
 
-  auto *head = new Node<typename RandomIt::value_type>(std::move(*first), nullptr);
+  auto* head = new Node<typename RandomIt::value_type>(std::move(*first), nullptr);
+  head->next = head;
 
-  auto currentNode = head;
+  auto x = head;
 
-  for (int i = 1; i < size; i++) {
-    auto* new_node = new Node<typename RandomIt::value_type>(std::move(*(first + i)), nullptr);
-    currentNode->next = new_node;
-    currentNode = new_node;
+  for (auto it = first + 1; it != last; it++) {
+    x = (x->next = new Node(std::move(*it), head));
   }
 
-  currentNode->next = head;
+  while (x != x->next) {
+    for (int i = 1; i < step_size; i++)
+      x = x->next;
+    x->next = x->next->next;
+  }
+
 }
 
-template <typename RandomIt>
-void MakeJosephusPermutation3(RandomIt first, RandomIt last, uint32_t step_size) {
 
-}
 
 vector<int> MakeTestVector() {
   vector<int> numbers(10);
@@ -118,10 +143,13 @@ void TestAvoidsCopying() {
 }
 
 int main() {
+  vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-  TestRunner tr;
-  RUN_TEST(tr, TestIntVector);
-  RUN_TEST(tr, TestAvoidsCopying);
+  //MakeJosephusPermutation(v.begin(), v.end(), 5);
+  MakeJosephusPermutation(v.begin(), v.end(), 2);
+  //cout << v << endl;
+
+  cout << v << endl;
 
   return 0;
 }
